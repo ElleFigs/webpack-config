803e3200d52917e5190d857eb7508373d72e50cc
= <=>
2021-06-19 09:28:36 -0400

    Uploading starter files for project template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const elvenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
+    refinedMoonstone: 6
+}
+
+const elevenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
     refinedMoonstone: 4
 }
 
 console.log(elvenShieldRecipe)
+console.log(elevenGauntletsRecipe)
